d993fc1624f65066ef1049e25b68e659bedd108b
Mark Donatelli <markwdonatelli@yahoo.com>
2022-06-06 16:48:12 -0400

    added getters

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -25,10 +25,20 @@
         :class="{ 'is-active': showMobileNav }"
       >
         <div class="navbar-end">
-          <RouterLink to="/" class="navbar-item" active-class="is-active">
+          <RouterLink
+            to="/"
+            @click="showMobileNav = false"
+            class="navbar-item"
+            active-class="is-active"
+          >
             Notes
           </RouterLink>
-          <RouterLink to="/stats" class="navbar-item" active-class="is-active">
+          <RouterLink
+            to="/stats"
+            @click="showMobileNav = false"
+            class="navbar-item"
+            active-class="is-active"
+          >
             Stats
           </RouterLink>
         </div>
--- src/views/ViewStats.vue
+++ src/views/ViewStats.vue
@@ -1,5 +1,39 @@
 <template>
   <div class="stats">
-    <h1>Stats</h1>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>Stat</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Number of Notes</td>
+          <td>{{ numberOfNotes }}</td>
+        </tr>
+        <tr>
+          <td>Number of Character (of all notes)</td>
+          <td>{{ numberOfCharacters }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script setup>
+/*
+ imports
+*/
+
+import { useStoreNotes } from '@/stores/storeNotes';
+
+/*
+store
+*/
+
+const storeNotes = useStoreNotes();
+
+let numberOfNotes = storeNotes.totalNotesCount;
+let numberOfCharacters = storeNotes.totalNotesCharacterCount;
+</script>
