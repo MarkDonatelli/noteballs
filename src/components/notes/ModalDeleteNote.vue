a92d55bd81fc79253d19172a8af87cfd56908570
Mark Donatelli <markwdonatelli@yahoo.com>
2022-06-07 11:39:55 -0400

    delete note modal setup

---
+++ src/components/notes/ModalDeleteNote.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="modal is-active p-2">
+    <div class="modal-background"></div>
+    <div class="modal-card" ref="modalDeleteNoteRef">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Delete Note?</p>
+        <button @click="closeModal" class="delete" aria-label="close"></button>
+      </header>
+      <section class="modal-card-body">
+        Are you sure you want to delete this note?
+      </section>
+      <footer class="modal-card-foot is-justify-content-flex-end">
+        <button @click="closeModal" class="button">Cancel</button>
+        <button class="button is-danger">Delete</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script setup>
+/*
+imports
+*/
+import { ref } from 'vue';
+import { onClickOutside } from '@vueuse/core';
+
+/*
+props
+*/
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+/*
+emits
+*/
+
+const emit = defineEmits(['update:modelValue']);
+
+/*
+close modal
+*/
+
+const modalDeleteNoteRef = ref(null);
+
+const closeModal = () => {
+  emit('update:modelValue', false);
+};
+
+onClickOutside(modalDeleteNoteRef, closeModal);
+</script>
--- src/components/notes/Note.vue
+++ src/components/notes/Note.vue
@@ -13,12 +13,13 @@
         >Edit</RouterLink
       >
       <a
-        @click.prevent="storeNotes.deleteNote(props.note.id)"
+        @click.prevent="modals.deleteNote = true"
         href="#"
         class="card-footer-item"
         >Delete</a
       >
     </footer>
+    <ModalDeleteNote v-if="modals.deleteNote" v-model="modals.deleteNote" />
   </div>
 </template>
 
@@ -27,7 +28,8 @@
  imports
 */
 
-import { computed } from 'vue';
+import { computed, reactive } from 'vue';
+import ModalDeleteNote from '@/components/notes/ModalDeleteNote.vue';
 import { useStoreNotes } from '@/stores/storeNotes';
 
 /*
@@ -55,5 +57,12 @@ const characterLength = computed(() => {
   let length = props.note.content.length;
   let description = length <= 1 ? 'character' : 'characters';
   return `${length} ${description}`;
+});
+
+/*
+modals
+*/
+const modals = reactive({
+  deleteNote: false,
 });
 </script>
